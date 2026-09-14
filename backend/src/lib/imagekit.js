faacc477247e98e9919f4d5fe3dfe52304fb4f22
Subhradeep Sikder <isdsikder@gmail.com>
2026-09-14 22:21:21 +0530

    new

--- backend/src/lib/imagekit.js
+++ backend/src/lib/imagekit.js
@@ -5,7 +5,7 @@ const imagekit = new ImageKit({ privateKey: process.env.IMAGEKIT_PRIVATE_KEY });
 function hasImageKitConfig() {
   return Boolean(process.env.IMAGEKIT_PRIVATE_KEY);
 }
-
+//new
 // originalName= "My Photo (1).png"
 // result: "chat-1749300000000-My_Photo__1_.png"
 // this helper makes a safe, unique filename for uploaded files.
